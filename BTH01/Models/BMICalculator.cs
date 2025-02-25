a23bc95960e1073c79842000d53d137c756557f4
hoduyanh1810 <aduy4046@gmail.com>
2025-02-25 21:41:47 +0700

    BTH_01

---
+++ BTH01/Models/BMICalculator.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BMICalculatorApp.Models
+{
+    public class BMICalculator
+    {
+        [Required(ErrorMessage = "Vui lòng nhập cân nặng")]
+        [Range(1, 500, ErrorMessage = "Cân nặng phải từ 1 đến 500 kg")]
+        public double Weight { get; set; }  // Cân nặng (kg)
+
+        [Required(ErrorMessage = "Vui lòng nhập chiều cao")]
+        [Range(0.5, 2.5, ErrorMessage = "Chiều cao phải từ 0.5 đến 2.5 m")]
+        public double Height { get; set; }  // Chiều cao (m)
+
+        public double BMI { get; set; } // Chỉ số BMI
+
+        public string Category { get; set; } // Phân loại BMI
+    }
+}
